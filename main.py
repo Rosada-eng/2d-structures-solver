bd697c0e7cdc6eba5b9b2edaa33a07058035021a
Guilherme <guilhermer4@al.insper.edu.br>
2022-06-03 20:23:50 -0300

    Add initial files

---
+++ main.py
@@ -0,0 +1,14 @@
+from funcoesTermosol import importa, geraSaida, plota
+
+if __name__ == '__main__':
+    """
+    nn = numero de nos
+    N = matriz dos nos
+    nm = numero de membros
+    Inc = matriz de incidencia
+    nc = numero de cargas
+    F = vetor carregamento
+    nr = numero de restricoes
+    R = vetor de restrições
+    """
+    nn,N,nm,Inc,nc,F,nr,R = importa("entrada.xls")
